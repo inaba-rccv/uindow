9376bd32349f994ea7caf3f90aff2f40c47e866e
Inaba <1403744538@qq.com>
2025-05-28 15:35:51 +0800

    新增button组件属性

--- packages/components/button/index.vue
+++ packages/components/button/index.vue
@@ -7,20 +7,26 @@ const props = withDefaults(
     type?: UindowColorType
     disabled?: boolean
     circle?: boolean
+    square?: boolean
     plain?: boolean
+    activeShrink?: boolean
   }>(),
   {
     type: 'primary',
     disabled: false,
     circle: false,
+    square: false,
     plain: false,
+    activeShrink: false
   },
 )
 const classes = computed(() => [
   `ui-button--${props.type}`,
   {
     'circle': props.circle,
+    'square': props.square,
     'plain': props.plain,
+    'active-shrink': props.activeShrink,
   },
 ])
 </script>
