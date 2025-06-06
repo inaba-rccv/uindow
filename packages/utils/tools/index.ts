ff04bff2a444121c8f955d5f2b69881dc8048c97
Inaba <1403744538@qq.com>
2025-06-06 17:42:51 +0800

    添加备注

--- packages/utils/tools/index.ts
+++ packages/utils/tools/index.ts
@@ -1,7 +1,30 @@
+/**
+ * 添加 px 后缀
+ * @param x 数字
+ * @returns 带px后缀的字符串
+ */
 export function addPx(x: number): string {
   return `${String(x)}px`
 }
 
+/**
+ * 向class字符串添加一个class
+ */
+export function addClass(classString: string, className: string): string {
+  if (!classString) {
+    return className
+  }
+  if (classString.includes(className)) {
+    return classString
+  }
+  return `${classString} ${className}`.trim()
+}
+
+/**
+ * 获取元素的父级背景色
+ * @param element DOM 元素
+ * @returns 背景色hex字符串或null
+ */
 export function getParentBackgroundColor(element: HTMLElement): string | null {
   if (!element)
     return null
