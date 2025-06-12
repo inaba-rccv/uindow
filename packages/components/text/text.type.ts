16a0aaad0bc30e339ba556cb5a7b9a024ab0c298
Inaba <1403744538@qq.com>
2025-06-12 09:11:00 +0800

    修改文字动画类名

--- packages/components/text/text.type.ts
+++ packages/components/text/text.type.ts
@@ -1,6 +1,10 @@
-export type EnterAnimate =
-  'bounce' |
-  'fadeInUp' |
+export type AnimateType =
+  'bounceIn' |
+  'fadeUpIn' |
   'blurIn' |
   'eraser' | // 和typewriter互斥
   ''
+
+export type AnimateDefaultStyleType = {
+  [key in AnimateType]: any
+}
