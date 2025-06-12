0e4dd168a71d8f41c41d9b28cfeaf336ff58107b
Inaba <1403744538@qq.com>
2025-06-12 09:10:25 +0800

    图标修改

---
+++ packages/components/svg/IInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 200 200" width="200" height="200"
+  >
+    <path
+      d="
+        M 100 8
+        a 92 92 0 1 1 0 184 92 92 0 1 1 0 -184
+        z
+        M 100 140
+        a 8 8 0 1 0 0 16 8 8 0 1 0 0 -16
+        z
+        M 108 65
+        a 8 8 0 1 0 -16 0
+        l 0 50
+        a 8 8 0 1 0 16 0
+        z
+      "
+    />
+  </svg>
+</template>
