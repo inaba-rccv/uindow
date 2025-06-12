062896416723733706f728ff13a20d169d569b86
Inaba <1403744538@qq.com>
2025-06-12 09:11:33 +0800

    封装部分文字动画

---
+++ packages/components/text/text.ts
@@ -0,0 +1,26 @@
+import type { AnimateDefaultStyleType } from './text.type'
+
+export const TYPEWRITER_ANIMATION_DELAY = 30 // ms
+export const animateDefaultStyleMap: AnimateDefaultStyleType = {
+  'blurIn': {
+    animationTimingFunction: 'linear',
+    animationDuration: '1s',
+    display: 'inline',
+  },
+  'bounceIn': {
+    animationTimingFunction: 'linear',
+    animationDuration: '1s',
+  },
+  'fadeUpIn': {
+    animationTimingFunction: 'var(--timing-function-ease-out)',
+    animationDuration: '1s',
+  },
+  'eraser': {
+    animationTimingFunction: 'linear',
+    animationDuration: '3s',
+    opacity: 1,
+  },
+  '': {
+
+  },
+}
