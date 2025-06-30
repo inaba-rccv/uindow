76dec9cb4c0a1ec82400df4915a1bd7b4803f95c
Inaba <1403744538@qq.com>
2025-06-30 15:53:48 +0800

    修改npm包打包配置

--- packages/uindow/vite.config.ts
+++ packages/uindow/vite.config.ts
@@ -6,23 +6,19 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      // 指定入口文件
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'MyComponentLib', // UMD 构建时的全局变量名
-      fileName: format => `my-component-lib.${format}.js`,
+      name: 'uindow',
+      fileName: format => `uindow.${format}.js`,
     },
     rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
       external: ['vue'],
       output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           vue: 'Vue',
         },
       },
     },
-    // 生成类型声明文件
-    sourcemap: true,
+    sourcemap: false,
     minify: true,
   },
 })
